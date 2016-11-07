712b0a7a71a4cc75b08bf2b9bd350de565bd68fa
Alejandro Tardín <alejandro@tardin.com>
2016-11-07 22:41:16 +0100

    Removing elm warnings

--- src/Main.elm
+++ src/Main.elm
@@ -4,11 +4,13 @@ import View exposing(view)
 import Messages exposing(Msg)
 import Update exposing (update)
 
+init : (Model, Cmd Msg)
 init = ({
     searchTerm = "",
     songs = Present [],
     selectedSong = Nothing}, Cmd.none)
 
+main : Program Never
 main = App.program {
     init = init,
     view = view,
